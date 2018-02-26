9ede09e6f524e800a4673eff9a8dca54bffc3a3d
Steven Toth <stoth@ltnglobal.com>
2018-02-26 13:31:46 -0500

    Added the mux-rework build target

--- build.sh
+++ build.sh
@@ -80,6 +80,11 @@ elif [ "$1" == "ac3offset" ]; then
 	LIBKLVANC_TAG=vid.obe.1.1.5
 	LIBKLSCTE35_TAG=vid.obe.1.1.2
 	LIBMPEGTS_TAG=vid.libmpegts-obe-1.1.2
+elif [ "$1" == "mux-rework" ]; then
+	OBE_TAG=mux-rework
+	LIBKLVANC_TAG=vid.obe.1.1.5
+	LIBKLSCTE35_TAG=vid.obe.1.1.2
+	LIBMPEGTS_TAG=vid.libmpegts-obe-1.1.2
 else
 	echo "Invalid argument"
 	exit 1
