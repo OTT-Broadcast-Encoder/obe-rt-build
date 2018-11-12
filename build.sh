148177786506c330957a05a5a82d35d4f9a4333d
Steven Toth <stoth@kernellabs.com>
2018-11-12 11:01:49 -0500

    Rename hevc build target as vid.obe.2.0.0

--- build.sh
+++ build.sh
@@ -71,8 +71,8 @@ elif [ "$1" == "1.1.21" ]; then
 	LIBKLVANC_TAG=vid.obe.1.1.5
 	LIBKLSCTE35_TAG=vid.obe.1.1.2
 	LIBMPEGTS_TAG=hevc-dev
-elif [ "$1" == "hevc" ]; then
-	OBE_TAG=x265
+elif [ "$1" == "vid.obe.2.0.0" ]; then
+	OBE_TAG=2.0.0
 	LIBKLVANC_TAG=vid.obe.1.1.5
 	LIBKLSCTE35_TAG=vid.obe.1.1.2
 	LIBMPEGTS_TAG=hevc-dev
