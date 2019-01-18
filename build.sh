f11dad308b9f6bcc841bc6f3dcd466c6c4d679ea
Steven Toth <stoth@kernellabs.com>
2019-01-18 11:14:15 -0500

    Changed default 2.0 build to 2.0.7

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ elif [ "$1" == "vid.obe.1.1" ]; then
 	LIBKLSCTE35_TAG=vid.obe.1.1.2
 	LIBMPEGTS_TAG=vid.libmpegts-obe-1.1.2
 elif [ "$1" == "vid.obe.2.0" ]; then
-	OBE_TAG=vid.obe.2.0.5
+	OBE_TAG=vid.obe.2.0.7
 	LIBKLVANC_TAG=vid.obe.1.1.5
 	LIBKLSCTE35_TAG=vid.obe.1.1.2
 	LIBMPEGTS_TAG=hevc-dev
