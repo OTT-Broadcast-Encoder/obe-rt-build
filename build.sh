6df026266e764f3ac0d85d4a8f3fc30e1d298569
Steven Toth <stoth@kernellabs.com>
2019-03-27 12:32:04 -0400

    Added build tag 2.0.11

--- build.sh
+++ build.sh
@@ -45,8 +45,14 @@ elif [ "$1" == "vid.obe.1.1" ]; then
 	LIBKLVANC_TAG=vid.obe.1.1.5
 	LIBKLSCTE35_TAG=vid.obe.1.1.2
 	LIBMPEGTS_TAG=vid.libmpegts-obe-1.1.2
+elif [ "$1" == "vid.obe.2.0.11" ]; then
+	OBE_TAG=vid.obe.2.0.11
+	LIBKLVANC_TAG=vid.obe.1.1.5
+	LIBKLSCTE35_TAG=vid.obe.1.1.2
+	LIBMPEGTS_TAG=hevc-dev
+	BUILD_X265=1
 elif [ "$1" == "vid.obe.2.0" ]; then
-	OBE_TAG=vid.obe.2.0.8
+	OBE_TAG=2.0.0
 	LIBKLVANC_TAG=vid.obe.1.1.5
 	LIBKLSCTE35_TAG=vid.obe.1.1.2
 	LIBMPEGTS_TAG=hevc-dev
