02897928a4e7bfb9f41f85ed9f261269f816f798
Steven Toth <stoth@kernellabs.com>
2019-08-20 14:47:12 -0400

    v2.0.14

--- build.sh
+++ build.sh
@@ -70,6 +70,12 @@ elif [ "$1" == "vid.obe.2.0.12" ]; then
 	LIBKLSCTE35_TAG=vid.obe.1.1.2
 	LIBMPEGTS_TAG=hevc-dev
 	BUILD_X265=1
+elif [ "$1" == "vid.obe.2.0.14" ]; then
+	OBE_TAG=vid.obe.2.0.14
+	LIBKLVANC_TAG=vid.obe.1.2.1
+	LIBKLSCTE35_TAG=vid.obe.1.2.0
+	LIBMPEGTS_TAG=hevc-dev
+	BUILD_X265=1
 elif [ "$1" == "vid.obe.2.0" ]; then
 	OBE_TAG=2.0.0
 	LIBKLVANC_TAG=vid.obe.1.2.1
