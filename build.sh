f430f7bd2938f59fdd37c3f7deae5d546205ca20
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2024-06-04 11:29:53 +0200

    Cleanup build script

--- build.sh
+++ build.sh
@@ -11,18 +11,8 @@ LIBKLSCTE35_TAG=vid.obe.1.3.0
 LIBMPEGTS_TAG=hevc-dev
 X265_TAG=ef1c5205fc14d436b71b1459eba0c85fec0013b7
 X264_BITDEPTH=8
-JSONC_TAG=6c55f65d07a972dbd2d1668aab2e0056ccdd52fc
-BUILD_X265=1
-BUILD_VAAPI=1
-BUILD_LIBLTNTSTOOLS=1
 LIBLTNTSTOOLS_TAG=4fbb32125bc1ea095cf26a0f7b1b279082fdd592
-# Absolute path to the SDK. Fixme.
-BUILD_NDI=1
 NDI_SDK=$PWD/NDI/sdk
-# Absolute path to the SDK. Fixme.
-DEKTEC_DRV=$PWD/dektecsdk/2019.11.0/Drivers/DtPcie/Source/Linux
-DEKTEC_SDK=$PWD/dektecsdk/2019.11.0/DTAPI
-DEKTEC_SDK_INC=$PWD/dektecsdk/2019.11.0/DTAPI/Include
 
 
 if [ "$1" == "" ]; then
@@ -49,7 +39,6 @@ elif [ "$1" == "clean" ]; then
 	rm -rf  libzvbi
 	rm -rf  x265
 	rm -rf  libwebsockets
-	rm -rf  json-c
 	rm -rf  libltntstools
 	rm -f   tarball.tgz
 
@@ -101,63 +90,66 @@ if [ ! -d blackmagic-sdk ]; then
 fi
 
 
-if [ $BUILD_LIBLTNTSTOOLS -eq 1 ]; then
-	if [ ! -d libltntstools ]; then
-		git clone https://github.com/LTNGlobal-opensource/libltntstools.git
-		cd libltntstools && git checkout $LIBLTNTSTOOLS_TAG && cd ..
-	fi
+if [ ! -d libltntstools ]; then
+	git clone https://github.com/LTNGlobal-opensource/libltntstools.git
+
+	pushd libltntstools
+		git checkout $LIBLTNTSTOOLS_TAG
+	popd
 fi
 
-if [ $BUILD_NDI -eq 1 ]; then
+if [ ! -d NDI]; then
 	mkdir -p NDI
-	cd NDI
-	if [ ! -f InstallNDISDK_v4_Linux.tar.gz ]; then
-		wget --no-check-certificate https://downloads.ndi.tv/SDK/NDI_SDK_Linux/InstallNDISDK_v4_Linux.tar.gz
-	fi
-	if [ ! -f InstallNDISDK_v4_Linux.sh ]; then
-		tar zxf InstallNDISDK_v4_Linux.tar.gz
-	fi
-	if [ ! -d sdk ]; then
-		echo "Y" | ./InstallNDISDK_v4_Linux.sh >/dev/null
-		mv 'NDI SDK for Linux' sdk
-	fi
-	cd ..
+	pushd NDI
+		if [ ! -f InstallNDISDK_v4_Linux.tar.gz ]; then
+			wget --no-check-certificate https://downloads.ndi.tv/SDK/NDI_SDK_Linux/InstallNDISDK_v4_Linux.tar.gz
+		fi
+
+		if [ ! -f InstallNDISDK_v4_Linux.sh ]; then
+			tar -zxf InstallNDISDK_v4_Linux.tar.gz
+		fi
+
+		if [ ! -d sdk ]; then
+			echo "Y" | ./InstallNDISDK_v4_Linux.sh >/dev/null
+			mv 'NDI SDK for Linux' sdk
+		fi
+	popd
 fi
 
-if [ $BUILD_X265 -eq 1 ]; then
-	if [ ! -d x265 ]; then
-		git clone https://github.com/videolan/x265.git
-		cd x265
+if [ ! -d x265 ]; then
+	git clone https://github.com/videolan/x265.git
+
+	pushd x265
 		git checkout $X265_TAG
 		patch -p1 <../patches/0003-x265-sei-overflow.patch
 		patch -p1 <../patches/0004-x265-sei-additional-elements-segfault.patch
 		#patch -p1 <../patches/0005-x265-additional-recovery-points.patch
-		cd ..
-	fi
+	popd
 fi
 
 if [ ! -d libzvbi ]; then
 	git clone https://github.com/LTNGlobal-opensource/libzvbi.git
-	if [ "$LIBZVBI_TAG" != "" ]; then
-		cd libzvbi
+
+	pushd libzvbi
 		git checkout $LIBZVBI_TAG
 		patch -p1 <../patches/0000-libzvbi-remove-png-dep.patch
-		cd ..
-	fi
+	popd
 fi
 
 if [ ! -d libklvanc ]; then
 	git clone https://github.com/LTNGlobal-opensource/libklvanc.git
-	if [ "$LIBKLVANC_TAG" != "" ]; then
-		cd libklvanc && git checkout $LIBKLVANC_TAG && cd ..
-	fi
+
+	pushd libklvanc
+		git checkout $LIBKLVANC_TAG
+	popd
 fi
 
 if [ ! -d libklscte35 ]; then
 	git clone https://github.com/LTNGlobal-opensource/libklscte35.git
-	if [ "$LIBKLSCTE35_TAG" != "" ]; then
-		cd libklscte35 && git checkout $LIBKLSCTE35_TAG && cd ..
-	fi
+
+	pushd libklscte35
+		git checkout $LIBKLSCTE35_TAG
+	popd
 fi
 
 if [ ! -d obe-rt ]; then
@@ -168,24 +160,27 @@ fi
 
 if [ ! -d x264 ]; then
 	git clone https://github.com/OTT-Broadcast-Encoder/x264.git
+else
+	pushd x264 && git pull && popd
 fi
 
 if [ ! -d ffmpeg ]; then
 	git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
-	cd ffmpeg && git checkout $FFMPEG_TAG && cd ..
+	pushd ffmpeg && git checkout $FFMPEG_TAG && popd
 fi
 
 if [ ! -d libmpegts-obe ]; then
 	git clone https://github.com/LTNGlobal-opensource/libmpegts-obe.git
-	if [ "$LIBMPEGTS_TAG" != "" ]; then
-		cd libmpegts-obe && git checkout $LIBMPEGTS_TAG && cd ..
-	fi
+
+	pushd libmpegts-obe
+		git checkout $LIBMPEGTS_TAG
+	popd
 fi
 
 if [ ! -d libyuv ]; then
 	git clone https://github.com/LTNGlobal-opensource/libyuv.git
+
 	pushd libyuv
-		# Make sure we use a known-good version
 		git checkout $LIBYUV_TAG
 		patch -p1 <../patches/0002-libyuv-csc-height.patch
 	popd
@@ -195,60 +190,42 @@ if [ ! -d twolame ]; then
 	git clone https://github.com/OTT-Broadcast-Encoder/twolame.git
 fi
 
-if [ $BUILD_LIBLTNTSTOOLS -eq 1 ]; then
-	pushd libltntstools
-		if [ ! -f .skip ]; then
-			./autogen.sh --build
-			./configure --prefix=$PWD/../target-root/usr/local --enable-shared=no
-			make -j$JOBS
-			make install
-			touch .skip
-		fi
-	popd
-fi
+pushd libltntstools
+	if [ ! -f .skip ]; then
+		./autogen.sh --build
+		./configure --prefix=$PWD/../target-root/usr/local --enable-shared=no
+		make -j$JOBS
+		make install
+		touch .skip
+	fi
+popd
 
-if [ $BUILD_VAAPI -eq 1 ]; then
-	pushd vaapi
-		./build.sh
-		# Newer builds default to the iHD driver, use the haswell compatible driver.
-		# LIBVA_DRIVER_NAME=i965 ./vainfo
-	popd
-fi
+pushd vaapi
+	./build.sh
+	# Newer builds default to the iHD driver, use the haswell compatible driver.
+	# LIBVA_DRIVER_NAME=i965 ./vainfo
+popd
 
-
-if [ $BUILD_JSONC -eq 1 ]; then
-	pushd json-c
-		if [ ! -f .skip ]; then
-			./autogen.sh
-			./configure --prefix=$PWD/../target-root/usr/local --enable-shared=no
-			make -j$JOBS
-			make install
-			touch .skip
-		fi
-	popd
-fi
-
-if [ $BUILD_X265 -eq 1 ]; then
-	pushd x265/build/linux
-		if [ ! -f .skip ]; then
-			# Static only build
-			#./make-Makefiles.bash
-			cmake -DCMAKE_INSTALL_PREFIX=$PWD/../../../target-root/usr/local \
-				-G "Unix Makefiles" \
-				-DENABLE_SHARED:bool=off \
-				-DENABLE_LIBNUMA:bool=off \
-				../../source
-			make -j$JOBS
-			make install
-			#touch .skip
-		fi
-	popd
-fi
+pushd x265/build/linux
+	if [ ! -f .skip ]; then
+		# Static only build
+		#./make-Makefiles.bash
+		cmake -DCMAKE_INSTALL_PREFIX=$PWD/../../../target-root/usr/local \
+			-G "Unix Makefiles" \
+			-DENABLE_SHARED:bool=off \
+			-DENABLE_LIBNUMA:bool=off \
+			../../source
+		make -j$JOBS
+		make install
+		touch .skip
+	fi
+popd
 
 pushd libzvbi
 	if [ ! -f .skip ]; then
 		./configure --enable-shared=no --prefix=$PWD/../target-root/usr/local
-		make && make install
+		make -j$JOBS
+		make install
 		make install
 		touch .skip
 	fi
@@ -258,7 +235,8 @@ pushd libklvanc
 	if [ ! -f .skip ]; then
 		./autogen.sh --build
 		./configure --enable-shared=no --prefix=$PWD/../target-root/usr/local --enable-dep-curses=no
-		make && make install
+		make -j$JOBS
+		make install
 		make install
 		touch .skip
 	fi
@@ -270,7 +248,8 @@ pushd libklscte35
 		export CFLAGS="-I$PWD/../target-root/usr/local/include"
 		export LDFLAGS="-L$PWD/../target-root/usr/local/lib"
 		./configure --enable-shared=no --prefix=$PWD/../target-root/usr/local
-		make && make install
+		make -j$JOBS
+		make install
 		touch .skip
 	fi
 popd
@@ -278,7 +257,8 @@ popd
 pushd libmpegts-obe
 	if [ ! -f .skip ]; then
 		./configure --prefix=$PWD/../target-root/usr/local
-		make && make install
+		make -j$JOBS
+		make install
 		make install
 		touch .skip
 	fi
@@ -287,7 +267,8 @@ popd
 pushd twolame
 	if [ ! -f .skip ]; then
 		./configure --prefix=$PWD/../target-root/usr/local --enable-shared=no
-		make && make install
+		make -j$JOBS
+		make install
 		touch .skip
 	fi
 popd
@@ -295,8 +276,10 @@ popd
 pushd x264
 	if [ ! -f .skip ]; then
 		make clean
-		./configure --enable-static --disable-cli --prefix=$PWD/../target-root/usr/local --disable-lavf --disable-swscale --disable-opencl --bit-depth=$X264_BITDEPTH
-		make -j$JOBS && make install
+		./configure --enable-static --disable-cli --prefix=$PWD/../target-root/usr/local \
+			--disable-lavf --disable-swscale --disable-opencl --bit-depth=$X264_BITDEPTH
+		make -j$JOBS
+		make install
 		touch .skip
 	fi
 popd
@@ -320,7 +303,8 @@ if [ ! -f .skip ]; then
 #			--extra-cflags="-I/usr/local/cuda/include/" \
 #			--extra-ldflags=-L/usr/local/cuda/lib64/
 
-	make -j$JOBS && make install
+	make -j$JOBS
+	make install
 	unset CFLAGS
 	unset LDFLAGS
 	unset PKG_CONFIG_PATH
@@ -346,21 +330,23 @@ build_obe() {
 
 	pushd obe-rt
 	export CFLAGS="-I$PWD/../target-root/usr/local/include -I$BMSDK_DIR -no-pie"
+	export CFLAGS="$CFLAGS -I$NDI_SDK/include"
 	export LDFLAGS="-L$PWD/../target-root/usr/local/lib -no-pie"
+	export LDFLAGS="$LDFLAGS -L$NDI_SDK/lib/x86_64-linux-gnu"
 	export PKG_CONFIG_PATH=$PWD/../target-root/usr/local/lib/pkgconfig
-	if [ $BUILD_NDI -eq 1 ]; then
-		export CFLAGS="$CFLAGS -I$NDI_SDK/include"
-		export LDFLAGS="$LDFLAGS -L$NDI_SDK/lib/x86_64-linux-gnu"
-	fi
 
 	if [ -f autogen.sh ]; then
 		./autogen.sh --build
 	fi
+
 	./configure --prefix=$PWD/../target-root/usr/local
+
 	make clean
 	make -j$JOBS
 	make install
+
 	popd
+
 	cp target-root/usr/local/bin/obecli obecli
 }
 
